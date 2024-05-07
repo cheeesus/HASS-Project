cca97b4741e8cac1fee54b7bf3ed29bca75a1884
Hiba Bourezg <61593371+cheeesus@users.noreply.github.com>
2024-05-07 14:58:39 +0100

    Update flask_server_MQTT.py
    updated using MQTT only

--- MQTT/flask_server_MQTT.py
+++ MQTT/flask_server_MQTT.py
@@ -1,4 +1,3 @@
-
 import time
 import datetime
 import adafruit_dht
@@ -18,32 +17,20 @@ app.config['MQTT_USERNAME'] = 'mqtt_user'
 app.config['MQTT_PASSWORD'] = 'mqttuser'
 
 mqtt = Mqtt(app)
+    
+LedPIN = 16
+ButtonPIN = 17
+LightSensorPIN = 2
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags,rc):
     if rc == 0:
        print('Connected successfully')
        mqtt.subscribe('homeassistant/led/set')
+       mqtt.subscribe('homeassistant/data')
     else:
        print('Bad connection. Code:', rc)
        
-@mqtt.on_message()
-def handle_message(client, userdata, message):
-    if message.topic == 'homeassistant/led/set':
-        payload = message.payload.decode('utf-8')
-        print(payload)
-        state = payload
-        if payload == 'on':
-            led.on()
-        elif payload == 'off':
-            led.off()   
-    mqtt.publish('homeassistant/led/state', state)
-    
-    
-LedPIN = 16
-ButtonPIN = 17
-LightSensorPIN = 2
-
 SECRET_KEY = 'vErYsEcuREeEeEeSeCreTTTkEyyyyyyYyYyY'
 users= {
     'bourezg': 'passwordofbourezg'
@@ -55,6 +42,7 @@ led = LED(LedPIN)
 
 @app.route('/login', methods=['POST'])
 def login():
+    
     username = request.json.get('username')
     password = request.json.get('password')
     
@@ -67,17 +55,31 @@ def login():
     token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days = 30)},SECRET_KEY)
     
     return jsonify({'token': token}), 200
- 
-@app.route('/data')
-def data():
-
-    token = request.headers.get('Authorization')
-    if not token:
-        return jsonify({'message': 'Missing token'}), 401
-        
-    try: 
-        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        username = decoded_token['username']
+@app.route('/')
+def index():
+    
+    now = datetime.datetime.now()
+    timeString = now.strftime("%d/%m/%Y %H:%M")
+    data = {
+        'title' : 'Test Server',
+        'time' : timeString,
+    }
+    return render_template('index.html', **data)
+@mqtt.on_message()
+def handle_message(client, userdata, message):
+    
+    if message.topic == 'homeassistant/led/set':
+        payload = message.payload.decode('utf-8')
+        print(payload)
+        state = payload
+        if payload == 'on':
+            led.on()
+        elif payload == 'off':
+            led.off()   
+        mqtt.publish('homeassistant/led/state', state)
+    elif message.topic == 'homeassistant/data':
+        '''payload = message.payload.decode('utf-8')
+        print(payload)'''
         lux = sensor.light
         try:
             temp = dhtDevice.temperature
@@ -86,13 +88,8 @@ def data():
             print(err.args[0])
             return jsonify({'message': 'failed to retrieve sensor data'}), 500
         sensor_data = { 'lux' : lux, 'temp' : temp, 'humid': humid}
+        print(sensor_data)
         mqtt.publish('homeassistant/sensor', json.dumps(sensor_data))
-        return jsonify(sensor_data), 200
-    
-    except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'Token has expired'}), 401
-    except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid Token'}), 401
         
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
