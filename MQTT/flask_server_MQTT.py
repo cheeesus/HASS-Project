a243644f9028473dad569681dc6abfe566cd8f05
Hiba Bourezg <61593371+cheeesus@users.noreply.github.com>
2024-05-16 12:12:18 +0100

    Update flask_server_MQTT.py
    calculate round trip time to get the MQTT latency

--- MQTT/flask_server_MQTT.py
+++ MQTT/flask_server_MQTT.py
@@ -4,21 +4,22 @@ import datetime
 import adafruit_dht
 import board
 import jwt
-import math
+import math, threading
 from gpiozero import LED, Button, MotionSensor
 from grove.grove_light_sensor_v1_2 import GroveLightSensor
 from flask import Flask, jsonify, request
 from flask_mqtt import Mqtt
 import json
 
-a = 1.2
+a = 1/1.01
 b = -0.6
-c = 0.4
+c = 0.01/1.01
 
 Xn = 0.0
 Xn_1 = 0.0
 Xn_2 = 0.0
-En = 2.0
+En = 1.0
+En_toggle = False
 
 sensor_data_error = None
 app = Flask(__name__)
@@ -45,20 +46,27 @@ def handle_connect(client, userdata, flags,rc):
        mqtt.subscribe('homeassistant/data')
        mqtt.subscribe('homeassistant/led/set')
        mqtt.subscribe('homeassistant/params')
+       mqtt.subscribe('homeassistant/ack')
     else:
        print('Bad connection. Code:', rc)   
 
 dhtDevice = adafruit_dht.DHT11(board.D18, use_pulseio=False)
 sensor = GroveLightSensor(LightSensorPIN)
-PIR = MotionSensor(MotionSensorPIN)
-led = LED(LedPIN)
+'''PIR = MotionSensor(MotionSensorPIN)
+led = LED(LedPIN)'''
    
 @mqtt.on_message()
 def handle_message(client, userdata, message):
     global sensor_data_error
     if message.topic == 'homeassistant/params':
         payload = json.loads(message.payload.decode('utf-8'))
-        print(payload['a'])
+        #print(payload['a'])
+    elif message.topic == 'homeassistant/ack':
+        current_time = time.time()
+        payload = json.loads(message.payload.decode('utf-8'))
+        sent_time = payload['timestamp']
+        round_trip_time = current_time - sent_time
+        print(f'round_trip_time :{round_trip_time}' )
     elif message.topic == 'homeassistant/led/set':
         payload = message.payload.decode('utf-8')
         print(payload)
@@ -86,19 +94,29 @@ def handle_message(client, userdata, message):
         sensor_data = { 'lux' : lux, 'temp' : temp, 'humid': humid, 'motion' : motion}
         print(sensor_data)
         mqtt.publish('homeassistant/sensor', json.dumps(sensor_data))
-        
-def ISR(signum, frame):
+#second order model        
+'''def ISR(signum, frame):
    global Xn, Xn_1, Xn_2, En
    #print(Xn)
    val = { 'val': Xn }
    mqtt.publish('homeassistant/speedtest', json.dumps(val)) 
    Xn = a * Xn_1 + b * Xn_2 + c * En
    Xn_2 = Xn_1
-   Xn_1 = Xn        
+   Xn_1 = Xn     '''  
+#first order model
+def ISR(signum, frame):
+   global Xn, Xn_1, En
+   #print(Xn)
+   val = { 'val': Xn, 'timestamp': time.time() }
+   mqtt.publish('homeassistant/energy', json.dumps(val)) 
+   Xn = a * Xn_1 + c * En
+   Xn_1 = Xn
+   En = 1.0 if not En_toggle else 0.0
    
 signal.signal(signal.SIGALRM, ISR)
-signal.setitimer(signal.ITIMER_REAL, 1,1)
-
+signal.setitimer(signal.ITIMER_REAL, 0.01,0.01)
+ 
+ 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get('username')
@@ -114,15 +132,10 @@ def login():
     token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days = 30)},SECRET_KEY)
     
     return jsonify({'token': token}), 200
-'''@app.route('/')
-def index():
-    now = datetime.datetime.now()
-    timeString = now.strftime("%d/%m/%Y %H:%M")
-    data = {
-        'title' : 'Test Server',
-        'time' : timeString,
-    }
-    return render_template('index.html', **data)'''
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+   threading.Thread(target = lambda: app.run(debug=True, port=5001, host='0.0.0.0')).start()
+   while True:
+      time.sleep(50)
+      En_toggle = not En_toggle
